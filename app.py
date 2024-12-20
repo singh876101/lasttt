795b86ed3d59e734dd3140a7b7ec29a94f029705
singh876101 <sir830477@gmail.com>
2024-12-20 11:50:37 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+import requests
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
+TOKEN = "7714162630:AAFIy7UvMS9OdDOTj68b8GRxSk0MHitCeJE"
+WEBHOOK_URL = "https://boiling-aurora-sonusharma-9c2bf4bb.koyeb.app/"
 
+def set_webhook():
+    url = f"https://api.telegram.org/bot{TOKEN}/setWebhook"
+    data = {"url": WEBHOOK_URL}
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        print("Webhook set successfully")
+    else:
+        print(f"Failed to set webhook: {response.text}")
 
 if __name__ == "__main__":
-    app.run()
+    set_webhook()
+
